3bb14262aa9d86527041c3e013624b57a39fb160
Ben Gelb <ben.gelb@gmail.com>
2024-01-03 22:25:47 +0000

    make work si514

--- gpsdo.ino
+++ gpsdo.ino
@@ -1,7 +1,25 @@
-#include <Adafruit_SI5351.h>
 #include <Wire.h>
+#include <Adafruit_I2CDevice.h>
 
-Adafruit_SI5351 clockgen = Adafruit_SI5351();
+#define SI514_ADDR 0x55
+
+#define SI514_REG_LP 0
+#define SI514_REG_M_FRAC1 5
+#define SI514_REG_M_FRAC2 6
+#define SI514_REG_M_FRAC3 7
+#define SI514_REG_M_INT_FRAC 8
+#define SI514_REG_M_INT 9
+#define SI514_REG_HS_DIV 10
+#define SI514_REG_LS_HS_DIV 11
+#define SI514_REG_OE_STATE 14
+#define SI514_REG_RESET 128
+#define SI514_REG_CONTROL 132
+
+#define SI514_OUTPUT_ENABLE = 0x04
+#define SI514_RESET = 0x80
+#define SI514_CALIBRATE = 0x01
+
+Adafruit_I2CDevice i2c_dev = Adafruit_I2CDevice(SI514_ADDR);
 uint32_t cnt, last;
 
 void IRAM_ATTR isr() {
@@ -10,8 +28,8 @@ void IRAM_ATTR isr() {
 
 void setup() {
   delay(500);
-  //Serial.begin(9600);
-  delay(500);
+  Serial.begin(115200);
+  delay(5000);
   Serial.println("\n\n================================");
   Serial.print("Chip Model: ");
   Serial.println(ESP.getChipModel());
@@ -25,17 +43,18 @@ void setup() {
   Serial.println(ESP.getFlashChipSpeed());
 
   Serial.println("================================");
-  Serial.println("Si5351 Clockgen Test"); Serial.println("");
 
-  Wire.begin(36, 35);
-  if (clockgen.begin() != ERROR_NONE)
-  {
-    /* There was a problem detecting the IC ... check your connections */
-    Serial.print("Ooops, no Si5351 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+  Wire.begin(10, 11);
+  Serial.println("I2C address detection test");
+
+  if (!i2c_dev.begin()) {
+    Serial.print("Did not find device at 0x");
+    Serial.println(i2c_dev.address(), HEX);
+    while (1);
   }
+  Serial.print("Device found on address 0x");
+  Serial.println(i2c_dev.address(), HEX);
 
-  Serial.println("OK!");
   cnt=0;
   last=0;
   pinMode(1, INPUT);
