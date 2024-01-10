3413330b26d52b596025a4a997761b872143495d
Ben Gelb <ben.gelb@gmail.com>
2024-01-10 01:13:11 +0000

    fixes

--- gpsdo.ino
+++ gpsdo.ino
@@ -88,17 +88,17 @@ void small_freq_change(Adafruit_I2CDevice &si514_dev, int32_t ppb) {
   int_m = reg08 >> 5;
   int_m += reg09 << 3;
 
-  Serial.printf("int_m: %d frac_m: %d\n", int_m, frac_m);
-  Serial.printf("frac_m: %x\n", frac_m);
-  Serial.printf("%x %x %x %x %x\n", reg09, reg08, reg07, reg06, reg05);
+  //Serial.printf("int_m: %d frac_m: %d\n", int_m, frac_m);
+  //Serial.printf("frac_m: %x\n", frac_m);
+  //Serial.printf("%x %x %x %x %x\n", reg09, reg08, reg07, reg06, reg05);
 
-  dm = frac_m * (int64_t)ppb / (int64_t)1000000000;
-  dm += ((int64_t)int_m << 29) * (int64_t)ppb / (int64_t)1000000000;
+  dm = ((int64_t)frac_m * (int64_t)ppb / (int64_t)1000000000) + (((int64_t)int_m << 29) * (int64_t)ppb / (int64_t)1000000000);
 
-  Serial.printf("dm: %d\n", dm);
+  //Serial.printf("dm: %d\n", dm);
  
-  frac_m = frac_m + (dm & 0x1fffffff);
-  int_m = int_m + (dm >> 29);
+  // TODO: this is sketchy
+  frac_m = (int32_t)frac_m + dm;
+  //int_m = (int32_t)int_m + (dm >> 29);
 
   reg05 = frac_m & 0xff;
   reg06 = (frac_m >> 8) & 0xff;
@@ -106,9 +106,9 @@ void small_freq_change(Adafruit_I2CDevice &si514_dev, int32_t ppb) {
   reg08 = (frac_m >> 24) & 0x1f | ((int_m & 0x07) << 5);
   reg09 = (int_m >> 3);
 
-  Serial.printf("int_m: %d frac_m: %d\n", int_m, frac_m);
-  Serial.printf("%x\n", frac_m);
-  Serial.printf("%x %x %x %x %x\n", reg09, reg08, reg07, reg06, reg05);
+  //Serial.printf("int_m: %d frac_m: %d\n", int_m, frac_m);
+  //Serial.printf("%x\n", frac_m);
+  //Serial.printf("%x %x %x %x %x\n", reg09, reg08, reg07, reg06, reg05);
 
   byte_write(si514_dev, SI514_REG_M_FRAC1, reg05);
   byte_write(si514_dev, SI514_REG_M_FRAC2, reg06);
@@ -252,6 +252,7 @@ void loop() {
 
     // State machine
     if(cal_state == S_IDLE) {
+      Serial.printf("saw PPS! cnt=%d tick=%d ext_tick=%d last_full_tick=%d new_full_tick=%d dt=%d\n", cnt, tick, ext_tick, last_full_tick, new_full_tick, new_full_tick-last_full_tick);
       cal_pps_count += 1;
       if (cal_pps_count > 10) {
         Serial.printf("Starting cal cycle interval = %d sec.\n", cal_interval);
@@ -267,13 +268,12 @@ void loop() {
       }
     } else if(cal_state == S_ADJUST) {
       ppb = (int32_t)(((int64_t)10000000 * (int64_t)cal_interval - (int64_t)cal_clock_count) * (int64_t)100 / (int64_t)cal_interval);
-      Serial.printf("cal_clock_count = %d cal_interval= %d\n", cal_clock_count, cal_interval);
-      Serial.printf("Finished cal cycle interval = %d sec. Error = %d ppb.\n", cal_interval, ppb);
+      Serial.printf("Finished cal cycle interval = %d sec. Count = %d. Error = %d ppb.\n", cal_interval, cal_clock_count, ppb);
       Serial.printf("ppb: %x\n", ppb);
       small_freq_change(si514_dev, ppb);
       cal_state = S_IDLE;
       cal_pps_count = 0;
-      if(cal_interval < 1000) {
+      if(cal_interval < 100) {
         cal_interval *= 10;
       }
     } else {
